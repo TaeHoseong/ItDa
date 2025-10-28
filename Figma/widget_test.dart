f4bd44b7849cc17eb0c83b9eeedd40b2aa12f14a
wooth2 <w01062400207@gmail.com>
2025-10-28 14:18:32 +0900

    에셋 추가

---
+++ Figma/widget_test.dart
@@ -0,0 +1,442 @@
+      // Figma Flutter Generator Iphone161Widget - FRAME
+      Container(
+      width: 393,
+      height: 852,
+      decoration: BoxDecoration(
+          color : Color.fromRGBO(250, 250, 250, 1),
+  ),
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: 373,
+        left: 120,
+        child: Container(
+      width: 154,
+      height: 219,
+      decoration: BoxDecoration(
+          
+  ),
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: 200,
+        left: 25,
+        child: Container(
+        width: 103,
+        height: 19,
+        decoration: BoxDecoration(
+          color : Color.fromRGBO(0, 0, 0, 0.25),
+      borderRadius : BorderRadius.all(Radius.elliptical(103, 19)),
+  )
+      )
+      ),Positioned(
+        top: 0,
+        left: 0,
+        child: null
+      ),Positioned(
+        top: 80,
+        left: 25,
+        child: Container(
+        width: 103,
+        height: 95,
+        decoration: BoxDecoration(
+          color : Color.fromRGBO(255, 255, 255, 1),
+      borderRadius : BorderRadius.all(Radius.elliptical(103, 95)),
+  )
+      )
+      ),Positioned(
+        top: 111,
+        left: 59,
+        child: null
+      ),Positioned(
+        top: 120,
+        left: 72,
+        child: null
+      ),
+        ]
+      )
+    )
+      ),Positioned(
+        top: 162,
+        left: 35,
+        child: Container(
+      width: 323,
+      height: 210.572998046875,
+      decoration: BoxDecoration(
+          
+  ),
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: 0,
+        left: 0,
+        child: null
+      ),Positioned(
+        top: 33,
+        left: 29,
+        child: Text('태희 님은 잘 못 먹는 음식이 있나요?', textAlign: TextAlign.left, style: TextStyle(
+        color: Color.fromRGBO(0, 0, 0, 1),
+        fontFamily: 'Ownglyph ryuttung',
+        fontSize: 22,
+        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+        fontWeight: FontWeight.normal,
+        height: 1
+      ),)
+      ),
+        ]
+      )
+    )
+      ),Positioned(
+        top: 663,
+        left: 24,
+        child: Container(
+      width: 346,
+      height: 45,
+      
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: 0,
+        left: 0,
+        child: Container(
+        width: 346,
+        height: 45,
+        decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(100),
+            topRight: Radius.circular(100),
+            bottomLeft: Radius.circular(100),
+            bottomRight: Radius.circular(100),
+          ),
+      color : Color.fromRGBO(227, 227, 227, 1),
+  )
+      )
+      ),Positioned(
+        top: 10,
+        left: 30,
+        child: Text('페르소나에게 말하기···', textAlign: TextAlign.left, style: TextStyle(
+        color: Color.fromRGBO(99, 59, 72, 1),
+        fontFamily: 'SUIT',
+        fontSize: 18,
+        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+        fontWeight: FontWeight.normal,
+        height: 1
+      ),)
+      ),
+        ]
+      )
+    )
+      ),Positioned(
+        top: 61,
+        left: 276,
+        child: Container(
+      decoration: BoxDecoration(
+          
+  ),
+      padding: EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        
+        children: <Widget>[
+          Container(
+      width: 34,
+      height: 29,
+      decoration: BoxDecoration(
+          
+  ),
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: 0.26574432849884033,
+        left: -8.049985353864031e-7,
+        child: Container(
+      width: 34,
+      height: 28.468509674072266,
+      
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: 0,
+        left: 0,
+        child: SvgPicture.asset(
+        'assets/images/vector.svg',
+        semanticsLabel: 'vector'
+      );
+      ),
+        ]
+      )
+    )
+      ),
+        ]
+      )
+    ), SizedBox(width : 16),
+Container(
+      width: 29,
+      height: 31,
+      decoration: BoxDecoration(
+          color : Color.fromRGBO(255, 255, 255, 1),
+  ),
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: 0.5438602566719055,
+        left: 0.6349848508834839,
+        child: SvgPicture.asset(
+        'assets/images/vector.svg',
+        semanticsLabel: 'vector'
+      );
+      ),Positioned(
+        top: 11.013157844543457,
+        left: 10.06944465637207,
+        child: SvgPicture.asset(
+        'assets/images/vector.svg',
+        semanticsLabel: 'vector'
+      );
+      ),
+        ]
+      )
+    ),
+
+        ],
+      ),
+    )
+      ),Positioned(
+        top: 762,
+        left: -7.105427357601002e-15,
+        child: Container(
+      width: 393,
+      height: 90,
+      
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: 0,
+        left: 0,
+        child: Container(
+      width: 393,
+      height: 90,
+      
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: 0,
+        left: 0,
+        child: Container(
+        width: 393,
+        height: 90,
+        decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(25),
+            topRight: Radius.circular(25),
+            bottomLeft: Radius.circular(0),
+            bottomRight: Radius.circular(0),
+          ),
+      color : Color.fromRGBO(255, 255, 255, 1),
+  )
+      )
+      ),
+        ]
+      )
+    )
+      ),Positioned(
+        top: 7,
+        left: 24,
+        child: Container(
+        width: 65,
+        height: 49,
+        decoration: BoxDecoration(
+          color : Color.fromRGBO(217, 217, 217, 1),
+  )
+      )
+      ),Positioned(
+        top: 18.400390625,
+        left: 43.400390625,
+        child: null
+      ),Positioned(
+        top: 7,
+        left: 117,
+        child: Container(
+        width: 65,
+        height: 49,
+        decoration: BoxDecoration(
+          color : Color.fromRGBO(255, 255, 255, 1),
+  )
+      )
+      ),Positioned(
+        top: 18,
+        left: 135,
+        child: Container(
+      width: 29.850000381469727,
+      height: 27.136363983154297,
+      decoration: BoxDecoration(
+          color : Color.fromRGBO(255, 255, 255, 1),
+  ),
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: -2.384185791015625e-7,
+        left: 0,
+        child: SvgPicture.asset(
+        'assets/images/vector.svg',
+        semanticsLabel: 'vector'
+      );
+      ),
+        ]
+      )
+    )
+      ),Positioned(
+        top: 7,
+        left: 303,
+        child: Container(
+        width: 65,
+        height: 49,
+        decoration: BoxDecoration(
+          color : Color.fromRGBO(217, 217, 217, 1),
+  )
+      )
+      ),Positioned(
+        top: 18,
+        left: 321,
+        child: Container(
+      width: 30.042856216430664,
+      height: 26.961538314819336,
+      decoration: BoxDecoration(
+          color : Color.fromRGBO(255, 255, 255, 1),
+  ),
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: 9.009190559387207,
+        left: 6.868496894836426,
+        child: SvgPicture.asset(
+        'assets/images/vector.svg',
+        semanticsLabel: 'vector'
+      );
+      ),Positioned(
+        top: 1.1554948091506958,
+        left: 1.2838830947875977,
+        child: SvgPicture.asset(
+        'assets/images/vector.svg',
+        semanticsLabel: 'vector'
+      );
+      ),
+        ]
+      )
+    )
+      ),Positioned(
+        top: 7,
+        left: 210,
+        child: Container(
+        width: 65,
+        height: 49,
+        decoration: BoxDecoration(
+          color : Color.fromRGBO(255, 255, 255, 1),
+  )
+      )
+      ),Positioned(
+        top: 18,
+        left: 227,
+        child: Container(
+      width: 31.008800506591797,
+      height: 26.992340087890625,
+      decoration: BoxDecoration(
+          color : Color.fromRGBO(255, 255, 255, 1),
+  ),
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: 0.020417433232069016,
+        left: 0.028784209862351418,
+        child: SvgPicture.asset(
+        'assets/images/vector.svg',
+        semanticsLabel: 'vector'
+      );
+      ),
+        ]
+      )
+    )
+      ),
+        ]
+      )
+    )
+      ),Positioned(
+        top: 0,
+        left: 0,
+        child: Container(
+      width: 393,
+      height: 852,
+      decoration: BoxDecoration(
+          color : Color.fromRGBO(255, 255, 255, 1),
+  ),
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: 838,
+        left: 130,
+        child: Container(
+        width: 134,
+        height: 5,
+        decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(100),
+            topRight: Radius.circular(100),
+            bottomLeft: Radius.circular(100),
+            bottomRight: Radius.circular(100),
+          ),
+      color : Color.fromRGBO(0, 0, 0, 1),
+  )
+      )
+      ),Positioned(
+        top: 0,
+        left: 0,
+        child: null
+      ),Positioned(
+        top: 21.776519775390625,
+        left: 55,
+        child: Container(
+      width: 302.3280334472656,
+      height: 12.377750396728516,
+      
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: 0.5568100214004517,
+        left: 276,
+        child: null
+      ),Positioned(
+        top: 0,
+        left: 0,
+        child: null
+      ),
+        ]
+      )
+    )
+      ),Positioned(
+        top: 21.776519775390625,
+        left: 55,
+        child: Container(
+      width: 302.3280334472656,
+      height: 12.377750396728516,
+      
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: 0.5568100214004517,
+        left: 276,
+        child: null
+      ),Positioned(
+        top: 0,
+        left: 0,
+        child: null
+      ),
+        ]
+      )
+    )
+      ),
+        ]
+      )
+    )
+      ),
+        ]
+      )
+    )
